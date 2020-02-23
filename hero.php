5f6a02102abc17073d4be95cbcfdd30785ebbc0a
Stephen C. Sanders <scsandman6496@gmail.com>
2020-02-22 22:02:20 -0500

    added generic logo and hero section with hero text overlay

---
+++ hero.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * @package WordPress
+ * @subpackage Baesick-Theme
+ */
+?>
+<div class="hero-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="hero-text text-center">
+                    <h1>Some Information</h1>
+                    <p>Even More information concerning this website</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ main-content.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * @package WordPress
+ * @subpackage Baesick-Theme
+ */
+?>
+
+   <div class="content">
+     <div class="two-thirds column alpha">
+       <div class="main"> 
+            
+      </div>  <!-- End Main -->
+    </div>  <!-- End two-thirds column -->
+  </div><!-- End Content -->
+    
