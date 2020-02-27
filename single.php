a40f2e434dda26a2da11a7a903b98ad3e8628bce
Stephen C. Sanders <scsandman6496@gmail.com>
2020-02-26 23:32:07 -0500

    created main-page page template and linked it to default page template, allowed nav links to be turned into buttons, other minor changes

--- single.php
+++ single.php
@@ -2,31 +2,8 @@
 /**
  * @package WordPress
  * @subpackage Baesick-Theme
- * This is the default page template
  */
- get_header();
-?>
-<?php while ( have_posts() ) : the_post(); ?>
 
-<div class="page-hero-section">
-    <div class="container">
-        <h1 class="page-header"><?php the_title_attribute(); ?></h1>
-    </div>
-</div>
-<div class="page-info">
-    <div class="container">
-        <main class="page" role="main">
-            <div class="bcrumb-nav">
-                <?php if( function_exists('yoast-breadcrumb') )
-                    { yoast_breadcrumb('<p class="bcrumb">', '</p>'); } ?>
-            </div>
-            <article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
-                <?php the_post_thumbnail(); ?>
-                <?php the_content(); ?>
-                <?php edit_post_link(); ?>
-            </article>
-        </main>
-    </div>
-</div>
-<?php endwhile; ?>
+<?php get_template_part( 'main-page', 'single' ); // main page template ?>
+                
 <?php get_footer(); ?>
