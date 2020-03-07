34c03e844de49cea98d1a949a883b2ef048d5dff
Stephen C. Sanders <scsandman6496@gmail.com>
2020-03-06 20:36:37 -0500

    multiple styling changes, removed layout.css from header

--- functions.php
+++ functions.php
@@ -39,11 +39,9 @@ function Wps_load_styles() {
 
 	wp_register_style( 'skeleton-style', get_template_directory_uri() . '/style.css');
 	wp_register_style( 'skeleton-base', get_template_directory_uri() . '/stylesheets/base.css');
-	wp_register_style( 'skeleton-layout', get_template_directory_uri() . '/stylesheets/layout.css');
 
 	wp_enqueue_style( 'skeleton-base' );
 	wp_enqueue_style( 'skeleton-style' );
-	wp_enqueue_style( 'skeleton-layout' );
 
 }
 add_action('wp_enqueue_scripts', 'Wps_load_styles');
--- header.php
+++ header.php
@@ -39,8 +39,11 @@
 	================================================== -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script> <!-- jQuery -->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script> <!-- Popper.js for Bootstrap -->
-    <script src="https://kit.fontawesome.com/a35f1d18e7.js" crossorigin="anonymous"></script> <!-- Font Awesome -->
-
+    <script src="js/scripts.js"></script> <!-- Theme scripts -->
+    
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/a35f1d18e7.js" crossorigin="anonymous"></script>
+    
 	<!-- Stylesheets
 	================================================== -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/stylesheets/base-styles.css"> <!-- Theme base styles -->
