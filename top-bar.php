e56a17cdbe95da0660e2930874389ede056253b7
Stephen C. Sanders <scsandman6496@gmail.com>
2020-03-21 00:59:59 -0400

    changes to top-bar, nav logo, cta

--- top-bar.php
+++ top-bar.php
@@ -9,10 +9,10 @@
         <div class="container">
             <div class="row text-center">
                 <div class="col-md-6">
-                    <p>A generic line about the uniqueness of this service</p>
+                    <p class="top-bar-info">A generic line about the uniqueness of this service</p>
                 </div>
                 <div class="col-md-6">
-                    <p class="social-icons"><a href="https:www.facebook.com" target="_blank"><i class="fab fa-facebook-square"><span class="seo-icon">F</span></i></a><span class="soc-pad"></span><a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"><span class="seo-icon">I</span></i></a><span class="soc-pad"></span><a href="https://www.twitter.com" target="_blank"><i class="fab fa-twitter-square"><span class="seo-icon">T</span></i></a></p>
+                    <p class="social-icons"><a href="https:www.facebook.com" target="_blank"><i class="fab fa-facebook-square fa-2x"><span class="seo-icon">F</span></i></a><span class="soc-pad"></span><a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram fa-2x"><span class="seo-icon">I</span></i></a><span class="soc-pad"></span><a href="https://www.twitter.com" target="_blank"><i class="fab fa-twitter-square fa-2x"><span class="seo-icon">T</span></i></a></p>
                 </div>
             </div>
         </div>
